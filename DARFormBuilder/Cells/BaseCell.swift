65eda4729155f925a3aad38f9c922c1994ee4226
Viktor Ten <viktor.ten@me.com>
2018-01-18 18:07:58 +0600

    Public access level for separatorStyle

--- DARFormBuilder/Cells/BaseCell.swift
+++ DARFormBuilder/Cells/BaseCell.swift
@@ -26,7 +26,7 @@ public class BaseCell: UITableViewCell {
         return config.cellPadding
     }
     
-    var separatorStyle: SeparatorStyle = .hairline
+    public var separatorStyle: SeparatorStyle = .hairline
     
     var shouldHideSeparator: Bool {
         return false
